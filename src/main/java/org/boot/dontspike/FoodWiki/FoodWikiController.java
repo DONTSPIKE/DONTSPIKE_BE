72a99c2e56be52d876cfcf5fc8c3592d98677cb3
Kseonjeong <sj030205@naver.com>
2024-08-01 23:11:51 +0900

    음식관리, 푸드위키2

--- src/main/java/org/boot/dontspike/FoodWiki/FoodWikiController.java
+++ src/main/java/org/boot/dontspike/FoodWiki/FoodWikiController.java
@@ -4,6 +4,7 @@ import org.boot.dontspike.Food.FoodService;
 import org.boot.dontspike.DTO.FoodDetailDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="https://localhost:5173")
 public class FoodWikiController {
     @Autowired
     private FoodService foodService;
