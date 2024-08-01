b4ec7f49593c29bcac68c4f89f86328bd6431861
Kseonjeong <sj030205@naver.com>
2024-08-01 23:11:51 +0900

    음식관리, 푸드위키2

--- src/main/java/org/boot/dontspike/BloodSugar/BloodSugarRepository.java
+++ src/main/java/org/boot/dontspike/BloodSugar/BloodSugarRepository.java
@@ -2,6 +2,8 @@ package org.boot.dontspike.BloodSugar;
 
 import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,4 +12,6 @@ import java.util.List;
 
 public interface BloodSugarRepository extends JpaRepository<BloodSugar, Long> {
     List<BloodSugar> findByUserIdAndRecordDateAfter(Long userId, LocalDateTime recordDate);
+    @Query("SELECT b FROM BloodSugar b WHERE b.user.id = :userId AND b.recordDate BETWEEN :startDate AND :endDate")
+    List<BloodSugar> findByUserIdAndDateRange(@Param("userId") Long userId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
