e9721e1278af1de0bc8ab668317600696619355a
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-08-02 21:17:39 +0900

    feat : 혈당입력 api 생성

--- src/main/java/org/boot/dontspike/BloodSugar/BloodSugarRepository.java
+++ src/main/java/org/boot/dontspike/BloodSugar/BloodSugarRepository.java
@@ -1,5 +1,6 @@
 package org.boot.dontspike.BloodSugar;
 
+import org.boot.dontspike.User.User;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,9 +10,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public interface BloodSugarRepository extends JpaRepository<BloodSugar, Long> {
     List<BloodSugar> findByUserIdAndRecordDateAfter(Long userId, LocalDateTime recordDate);
     @Query("SELECT b FROM BloodSugar b WHERE b.user.id = :userId AND b.recordDate BETWEEN :startDate AND :endDate")
     List<BloodSugar> findByUserIdAndDateRange(@Param("userId") Long userId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+    Optional<BloodSugar> findByUserAndRecordDate(User user, LocalDateTime recordDate);
 }
