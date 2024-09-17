9b31a0399d4e00493c8d2a6913969514637ca5ba
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-09-18 00:56:55 +0900

    feat : UserController

--- src/main/java/org/boot/dontspike/User/UserController.java
+++ src/main/java/org/boot/dontspike/User/UserController.java
@@ -1,5 +1,6 @@
 package org.boot.dontspike.User;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.boot.dontspike.Exception.InvalidCredentialsException;
 import org.boot.dontspike.Exception.UsernameAlreadyExistsException;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,9 +17,15 @@ import java.util.Map;
 @CrossOrigin(origins = "*")
 public class UserController {
 
-    @GetMapping("/api/login")
-    public ResponseEntity<String> loginPage() {
-        return ResponseEntity.ok("로그인 성공");
+    @GetMapping("/oauth2/authorization/naver")
+    public void redirectToNaverLogin(HttpServletResponse response) throws IOException {
+        // Naver OAuth2 로그인으로 리다이렉트
+        response.sendRedirect("/oauth2/authorization/naver"); // Spring Security가 설정한 리다이렉트 URL로 이동
     }
 
+    @GetMapping("/oauth2/authorization/google")
+    public void redirectToGoogleLogin(HttpServletResponse response) throws IOException {
+        // Google OAuth2 로그인으로 리다이렉트
+        response.sendRedirect("/oauth2/authorization/google"); // Spring Security가 설정한 리다이렉트 URL로 이동
+    }
 }
