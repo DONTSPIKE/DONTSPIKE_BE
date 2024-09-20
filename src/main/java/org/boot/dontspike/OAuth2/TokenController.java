c869982330ff04d091d80f4a2632b95cace9cbee
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-09-20 22:32:23 +0900

    feat : getAccessToken

--- src/main/java/org/boot/dontspike/OAuth2/TokenController.java
+++ src/main/java/org/boot/dontspike/OAuth2/TokenController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.boot.dontspike.JWT.JWTUtil;
 import org.boot.dontspike.User.User;
 import org.boot.dontspike.User.UserRepository;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ public class TokenController {
         // 쿠키에서 accessToken 찾기
         String accessToken = null;
         Cookie[] cookies = request.getCookies();
+
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if ("accessToken".equals(cookie.getName())) {
@@ -37,8 +39,19 @@ public class TokenController {
             return ResponseEntity.badRequest().body("Access token not found in cookies");
         }
 
+        // 새로운 ResponseCookie 생성 및 설정
+        ResponseCookie responseCookie = ResponseCookie.from("access", accessToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(24 * 60 * 60)
+                .sameSite("None")
+                .build();
+
+        // 응답에 쿠키 추가
+        response.addHeader("Set-Cookie", responseCookie.toString());
+
         // AccessToken을 응답으로 반환
         return ResponseEntity.ok(accessToken);
     }
 }
-
